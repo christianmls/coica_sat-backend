01d452a7392690acc92c113212e3e8dfe88e07e3
jdanieln <josedanielnarvaez452@gmail.com>
2022-03-10 17:02:57 -0600

    fixDeployment

--- src/apps/coica-sat/backend/dependency-injection/index.ts
+++ src/apps/coica-sat/backend/dependency-injection/index.ts
@@ -4,6 +4,8 @@ const container = new ContainerBuilder();
 const loader = new YamlFileLoader(container);
 const env = process.env.NODE_ENV || 'dev';
 
+console.log(`${__dirname}/application_${env}.yaml`, '***');
+
 loader.load(`${__dirname}/application_${env}.yaml`);
 
 export default container;

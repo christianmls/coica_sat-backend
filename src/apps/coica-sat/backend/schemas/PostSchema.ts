8871117d3d9e211ebf10ffd2143aefe2dfd6c39e
jdanieln <josedanielnarvaez452@gmail.com>
2022-04-01 07:27:11 -0600

    postSchemaFixing

--- src/apps/coica-sat/backend/schemas/PostSchema.ts
+++ src/apps/coica-sat/backend/schemas/PostSchema.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export const PostSchema = Joi.object().keys({
-  description: Joi.string().email().required(),
-  images: Joi.array().items(Joi.string().required())
+  description: Joi.string().required(),
+  images: Joi.array().items(Joi.string()).required()
 });

700d8ce99fcaa02d4a7c141bb0ebf628c6da63b5
jdanieln <josedanielnarvaez452@gmail.com>
2022-04-28 12:33:16 -0600

    fixApplicationForMonitoring

--- src/apps/coica-sat/backend/schemas/ApplicationForMonitoringSchema.ts
+++ src/apps/coica-sat/backend/schemas/ApplicationForMonitoringSchema.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi';
 
 export const ApplicationForMonitoringSchema = Joi.object().keys({
+  id: Joi.string().required(),
   status: Joi.string().valid('SOLICIDATO', 'APROBADO', 'RECHAZADO').required(),
   date: Joi.date().required(),
   details: Joi.string().required(),
